2183a2c43a3283b956c68b929bdbc9d316799151
Kathya <kathyawhite00@gmail.com>
2020-07-16 13:42:38 -0500

    CompletedTacoParser

--- LoggingKata/Program.cs
+++ LoggingKata/Program.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.IO;
 using GeoCoordinatePortable;
+using System.ComponentModel.DataAnnotations;
 
 namespace LoggingKata
 {
@@ -17,16 +18,29 @@ namespace LoggingKata
 
             logger.LogInfo("Log initialized");
 
-            // use File.ReadAllLines(path) to grab all the lines from your csv file
-            // Log and error if you get 0 lines and a warning if you get 1 line
+            // use File.ReadAllLines(path) to grab all the lines from your csv file DONE
+            // Log and error if you get 0 lines and a warning if you get 1 line DONE
             var lines = File.ReadAllLines(csvPath);
+
+            if(lines.Length == 0)
+            {
+                logger.LogError("File doesn't contain anything!");
+            }
+
+            if(lines.Length == 1)
+            {
+                logger.LogWarning("File only contains one line");
+            }
+
 
             logger.LogInfo($"Lines: {lines[0]}");
 
-            // Create a new instance of your TacoParser class
+
+
+            // Create a new instance of your TacoParser class DONE
             var parser = new TacoParser();
 
-            // Grab an IEnumerable of locations using the Select command: var locations = lines.Select(parser.Parse);
+            // Grab an IEnumerable of locations using the Select command: var locations = lines.Select(parser.Parse); DONE
             var locations = lines.Select(parser.Parse).ToArray();
 
             // DON'T FORGET TO LOG YOUR STEPS
@@ -34,14 +48,46 @@ namespace LoggingKata
             // Now that your Parse method is completed, START BELOW ----------
 
             // TODO: Create two `ITrackable` variables with initial values of `null`. These will be used to store your two taco bells that are the farthest from each other.
-            // Create a `double` variable to store the distance
+            ITrackable TacoBell1 = null;
+            ITrackable TacoBell2 = null;
+            double largestDistance = 0;
 
-            // Include the Geolocation toolbox, so you can compare locations: `using GeoCoordinatePortable;`
+            
+            // Create a `double` variable to store the distance DONE
+
+            // Include the Geolocation toolbox, so you can compare locations: `using GeoCoordinatePortable;` DONE
 
             //HINT NESTED LOOPS SECTION---------------------
             // Do a loop for your locations to grab each location as the origin (perhaps: `locA`)
+            foreach (var locationA in locations)
+            {
+                
+                var corA = new GeoCoordinate();
+                corA.Latitude = locationA.Location.Latitude;
+                corA.Longitude = locationA.Location.Longitude;
 
-            // Create a new corA Coordinate with your locA's lat and long
+                foreach (var locationB in locations)
+                {
+                    
+                    var corB = new GeoCoordinate();
+                    corB.Latitude = locationB.Location.Latitude;
+                    corB.Longitude = locationB.Location.Longitude;
+
+                    var distanceBetween = corA.GetDistanceTo(corB);
+
+                    if(distanceBetween > largestDistance)
+                    {
+                        largestDistance = distanceBetween;
+                        Console.WriteLine(largestDistance);
+                        TacoBell1 = locationA;
+                        Console.WriteLine(TacoBell1.Name);
+                        TacoBell2 = locationB;
+                        Console.WriteLine(TacoBell2.Name);
+                    }
+
+                }
+            }
+            // Create a new corA Coordinate with your locA's lat and long DONE
 
             // Now, do another loop on the locations with the scope of your first loop, so you can grab the "destination" location (perhaps: `locB`)
 
@@ -52,7 +98,7 @@ namespace LoggingKata
 
             // Once you've looped through everything, you've found the two Taco Bells farthest away from each other.
 
-
+            Console.WriteLine($"{TacoBell1.Name} and {TacoBell2.Name} are the farthest apart and their distance is {largestDistance}");
             
         }
     }
--- LoggingKata/TacoParser.cs
+++ LoggingKata/TacoParser.cs
@@ -11,34 +11,49 @@
         {
             logger.LogInfo("Begin parsing");
 
-            // Take your line and use line.Split(',') to split it up into an array of strings, separated by the char ','
-            var cells = line.Split(',');
+            // Take your line and use line.Split(',') to split it up into an array of strings, separated by the char ',' DONE
+            var cells = line.Split(','); //KW this line is taking the method Split and using it on a line(line is a string) to create an array of strings
 
             // If your array.Length is less than 3, something went wrong
-            if (cells.Length < 3)
+
+            if (cells.Length < 3) //KW the if statement is checking if the statement is less than 3, then we would have to throw an error
             {
+                logger.LogError("too few elements");
                 // Log that and return null
                 // Do not fail if one record parsing fails, return null
-                return null; // TODO Implement
+                return null; // TODO Implement DONE //KW we returned null because the program didn't work / ITrackable is nullable
             }
 
+            var latitude = double.Parse (cells[0]); //double.Parse is changing the string from cells and changing it to a double (usually for long numbers with decimals)
+            var longitude = double.Parse(cells[1]);
+            var name = cells[2]; //on this var we are getting the name, and the last cells, but we are NOT changing the type
+            //we wrote the var above to tell the program what we wanted to do from the comments below
             // grab the latitude from your array at index 0
             // grab the longitude from your array at index 1
-            // grab the name from your array at index 2
+            // grab the name from your array at index 2 DONE
 
-            // Your going to need to parse your string as a `double`
+            // Your going to need to parse your string as a `double`DONE (line 27, 28)
             // which is similar to parsing a string as an `int`
 
-            // You'll need to create a TacoBell class
-            // that conforms to ITrackable
+            // You'll need to create a TacoBell class DONE
+            // that conforms to ITrackable DONE 
+            var tacoBell = new TacoBell();
+            tacoBell.Name = name;
 
-            // Then, you'll need an instance of the TacoBell class
-            // With the name and point set correctly
+            //below I declared location with a variable and then I set the properties of location
+            var Location = new Point();  //Point is used to define Latitude and Longitude or Coordinates
+            Location.Latitude = latitude; //when using dot notation I'm taking the properties from what I comformed
+            Location.Longitude = longitude;
+            tacoBell.Location = Location;
+            
+
+            // Then, you'll need an instance of the TacoBell class DONE
+            // With the name and point set correctly DONE
 
             // Then, return the instance of your TacoBell class
             // Since it conforms to ITrackable
 
-            return null;
+            return tacoBell; //after setting it all up, we want it to return
         }
     }
 }
